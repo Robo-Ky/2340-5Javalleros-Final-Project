4d0c58fea66b381758e998debe56deb1f9cdebd6
kanaganai <kanaganai@gmail.com>
2014-01-22 18:43:41 -0500

    Java Person 1 - Andrew

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,11 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+      String newInput, initialTwo, restOf;
+      initialTwo = input.substring(0,2)
+      restOf = input.substring(2);
+      newInput = restOf + initialTwo;
+	  return newInput;
 	}
 	
 	/**
